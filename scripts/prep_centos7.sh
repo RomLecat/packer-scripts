a17eef5b4c3c62d1a355dfe3d01e092c6ab3da12
Romain Lecat <romain@lecat.pro>
2019-08-27 23:35:33 +0200

    Basic CentOS 7 template
    Improvements to Packer vSphere
    
    Disable useless firmwares
    
    First working version

---
+++ scripts/prep_centos7.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Stop logging
+systemctl stop rsyslog
+
+# Log cleanup
+logrotate -f /etc/logrotate.conf
+rm -f /var/log/*-???????? /var/log/*.gz
+rm -f /var/log/dmesg.old
+rm -rf /var/log/anaconda
+cat /dev/null > /var/log/audit/audit.log
+cat /dev/null > /var/log/wtmp
+cat /dev/null > /var/log/lastlog
+cat /dev/null > /var/log/grubby
+
+# No history
+unset HISTFILE
+rm -f ~root/.bash_history
+history -c
+
+# Update
+yum update -y
+package-cleanup --oldkernels --count=1
+yum clean all
+
+# Network interface cleanup
+rm -f /etc/udev/rules.d/70*
+sed -i '/^(HWADDR|UUID)=/d' /etc/sysconfig/network-scripts/ifcfg-ens192
+
+# SSH Cleanup
+rm -f /etc/ssh/*key*
+history -c
+
+# Sysprep
+yum install -y initial-setup
+easy_install six
+hostnamectl set-hostname localhost.localdomain
+cat /dev/null > /etc/machine-id
