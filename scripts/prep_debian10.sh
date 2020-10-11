52a861634430cc6cb2a12ba7a8f641d56114503f
Romain Lecat <romain@lecat.io>
2020-10-11 21:44:00 +0200

    Debian 10 template

---
+++ scripts/prep_debian10.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Stop logging
+systemctl stop rsyslog
+
+# Log cleanup
+logrotate -f /etc/logrotate.conf
+rm -f /var/log/*-???????? /var/log/*.gz
+rm -f /var/log/dmesg.old
+cat /dev/null > /var/log/wtmp
+cat /dev/null > /var/log/lastlog
+
+# No history
+unset HISTFILE
+rm -f ~root/.bash_history
+history -c
+
+# Update
+apt update -y
+apt upgrade -y
+apt clean -y
+
+# Password-less sudo
+sed -i 's/\s*\(%sudo\s\+ALL=(ALL)\s*ALL\)/# \1/' /etc/sudoers
+sed -i 's/^#\s*\(%sudo\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' /etc/sudoers
+
+# SSH Cleanup
+rm -f /etc/ssh/*key*
+history -c
+
+# Sysprep
+cat /dev/null > /etc/machine-id
