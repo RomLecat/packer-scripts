7fea704d192bfa5c1a01fa6252e9d5a5569072d2
Romain Lecat <romain@lecat.io>
2022-06-07 12:06:49 +0200

    Template with cloud-init

--- kickstart/debian11/prep.sh
+++ kickstart/debian11/prep.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-apt autoremove -y avahi-daemon ifupdown fwupd
+apt autoremove -y avahi-daemon fwupd
 sed -i 's/#PermitRootLogin .*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/15-sudo-nopassword
+rm -f /etc/network/interfaces
+systemctl mask networking
 systemctl unmask systemd-timesyncd
 systemctl enable open-vm-tools systemd-networkd systemd-timesyncd systemd-resolved
 cat <<EOF > /etc/systemd/network/ens192.network
@@ -15,4 +17,8 @@ MTUBytes=1500
 [Network]
 DHCP=yes
 IPv6LinkLocalAddressGenerationMode=eui64
+EOF
+cat <<EOF > /etc/cloud/cloud.cfg.d/10-network.cfg
+network:
+  config: disabled
 EOF
