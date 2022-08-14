174fb0b3f3eef79e4a0c2c69600c258f635f5570
Romain Lecat <romain@lecat.io>
2022-08-14 15:52:56 +0200

    update package pin for osc deb

--- scripts/osc_debian11.sh
+++ scripts/osc_debian11.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Set root password
+echo "root:root" | chpasswd
+
 # Set apt preferences
 cat <<EOF > /etc/apt/preferences.d/90_default
 Package: *
 Pin: release a=stable
-Pin-Priority: 990
+Pin-Priority: 500
 
 Package: *
 Pin: release a=stable-updates
@@ -19,7 +22,25 @@ Pin: release o=Debian
 Pin-Priority: 50
 EOF
 
+cat <<EOF > /etc/apt/preferences.d/80_kernel
+Package: linux-image-amd64
+Pin: release o=Debian Backports
+Pin-Priority: 990
+
+Package: linux-image-cloud-amd64
+Pin: release o=Debian Backports
+Pin-Priority: 990
+
+Package: linux-headers-amd64
+Pin: release o=Debian Backports
+Pin-Priority: 990
+EOF
+
 cat <<EOF > /etc/apt/preferences.d/80_systemd
+Package: libsystemd0
+Pin: release o=Debian Backports
+Pin-Priority: 990
+
 Package: systemd
 Pin: release o=Debian Backports
 Pin-Priority: 990
@@ -32,19 +53,26 @@ Package: systemd-timesyncd
 Pin: release o=Debian Backports
 Pin-Priority: 990
 
-Package: libnss-resolve linux-image-amd64
+Package: libnss-systemd
+Pin: release o=Debian Backports
+Pin-Priority: 990
+
+Package: libpam-systemd
 Pin: release o=Debian Backports
 Pin-Priority: 990
 EOF
 
 # Upgrade
 apt update
+apt install -y systemd-timesyncd
 apt upgrade -y
-systemctl enable systemd-networkd systemd-timesyncd systemd-resolved
+apt autoremove -y
+systemctl enable --now systemd-networkd systemd-timesyncd systemd-resolved
 systemctl mask networking
 
 # Network
-sed -i 's/GRUB_CMDLINE_LINUX="console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200 consoleblank=0"/GRUB_CMDLINE_LINUX="console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200 consoleblank=0 net.ifnames=0"/g' /etc/default/grub
+sed -i "s/GRUB_CMDLINE_LINUX=\"\(.*\)\"/GRUB_CMDLINE_LINUX=\"\1 net.ifnames=0\"/" /etc/default/grub
+cat /etc/default/grub
 update-grub
 
 cat <<EOF > /etc/cloud/cloud.cfg.d/10_network.cfg
@@ -54,7 +82,7 @@ EOF
 
 cat <<EOF > /etc/systemd/network/eth0.network
 [Match]
-Name=ens3
+Name=eth0
 
 [Link]
 MTUBytes=1500
@@ -67,7 +95,6 @@ EOF
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/sudo-nopassword
 
 # Cleanup
-apt autoremove -y
 rm -f /etc/network/interfaces
 rm -f /etc/sudoers.d/90-cloud-init-users
 echo > /home/outscale/.ssh/authorized_keys 
